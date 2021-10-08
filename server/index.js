eeaa74e1f64ad1757dc0ba0a459686a26d904c24
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-10-08 20:57:44 +0300

    app.get

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hellow world");
+});
+
 app.listen(3001, () => {
   console.log("running on port ");
 });
