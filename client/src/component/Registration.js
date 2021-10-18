e22eeba683ac85d1f83110ed7bef955953c528fc
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-10-18 12:19:08 +0300

    take the user input

--- client/src/component/Registration.js
+++ client/src/component/Registration.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Axios from "axios";
 
 function Registration() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      alert("succsess");
+    });
+  };
   return (
     <div>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+        <Form.Group className="mb-3">
+          <Form.Label>Movie Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter email"
+            onChange={(e) => {
+              setMovieName(e.target.value);
+            }}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group className="mb-3">
+          <Form.Label>Review</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="review"
+            onChange={(e) => {
+              setReview(e.target.value);
+            }}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hellow world");
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "react-db",
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert = "INSERT INTO movie_reviews (name, review) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {});
+});
+
+// app.get("/", (req, res) => {
+//   const sqlInsert = "INSERT INTO movie_reviews (name, review) VALUES (?, ?);";
+//   db.query(sqlInsert, (err, result) => {
+//     res.send("hellow world");
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("running on port ");
