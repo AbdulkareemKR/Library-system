e07a585c0c70622c8a1289d78d64764f476a2554
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-10-22 19:19:30 +0300

    seperate the ports

--- server/index.js
+++ server/index.js
@@ -54,6 +54,8 @@ const app = express();
 //   });
 // });
 
-app.listen(process.env.PORT || 3001, () => {
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
   console.log("running on port ");
 });
