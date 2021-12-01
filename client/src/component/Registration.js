27f0e406fabc4639bd418311c7b64d50130700c7
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2021-12-01 20:23:26 +0300

    Update Registration.js

--- client/src/component/Registration.js
+++ client/src/component/Registration.js
@@ -102,7 +102,7 @@ function Registration() {
         <Button onClick={login}>login</Button>
         <div>
           {loginStatus && (
-            <Button onClick={userAuthenticated}>Check Authentication</Button>
+            <Button onClick={userAuthenticated}>Check hAuthentication</Button>
           )}
         </div>
       </Col>
