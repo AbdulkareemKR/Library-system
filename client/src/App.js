336e97b48aa73785e0cc93198251fbb7fa9212e1
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2021-12-10 12:22:46 +0300

    adding home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import HomePage from "./component/homePage/HomePage";
 import InformationCard from "./component/informationCard/InformationCard";
 import Registration from "./component/registration/Registration";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Registration />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/info" element={<InformationCard />} />
         </Routes>
       </Router>
